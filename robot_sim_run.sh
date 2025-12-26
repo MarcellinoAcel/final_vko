b34e6d589947b82e5796faa89b83977239c637aa
MarcellinoAcel <saragihmarcel34@gmail.com>
2025-12-26 19:14:28 +0700

    update arm

--- robot_sim_run.sh
+++ robot_sim_run.sh
@@ -2,6 +2,11 @@
 
 # Launch Gazebo
 gnome-terminal --tab --title="Gazebo" -- bash -c "source install/setup.bash;
-                                 ros2 launch robot_gazebo gazebo.launch.py;
+                                 ros2 launch robot_properties gazebo.launch.py;
                                 echo Press any key to close;
                                 read -n 1"
+
+# gnome-terminal --tab --title="Orb SLAM3" -- bash -c "source install/setup.bash;
+#                                  ros2 run orb_slam3_ros2 orb_slam3_node /home/marcel/final_vko/src/orb_slam3_ros2/vocabulary/ORBvoc.txt /home/marcel/final_vko/src/orb_slam3_ros2/config/rgb-d/RealSense_D435i.yaml;
+#                                 echo Press any key to close;
+#                                 read -n 1"jw
